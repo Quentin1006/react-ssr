98b82a53b8cceb9390449bd1d5f8268ca002accf
quentin.sahal <quentin.sahal@gmail.com>
2023-03-10 21:35:02 +0000

    small change

--- webpack.client.js
+++ webpack.client.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const { merge } = require("webpack-merge")
+const CopyPlugin = require("copy-webpack-plugin")
 const baseConfig = require("./webpack.base.js")
 
 /** @type { import('webpack').Configuration } */
@@ -10,7 +11,11 @@ const config = {
   devtool: "source-map",
   entry: "./src/client.tsx",
   target: "web",
-
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "assets", to: "." }],
+    }),
+  ],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
